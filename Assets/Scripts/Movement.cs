aa6176f3934869683071065d4aadf533ea32409e
JaniJuhaniKesola <jani.kesola@tuni.fi>
2024-01-22 10:17:57 +0200

    Movement scripts added

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private Transform groundCheck;
+    private LayerMask groundLayer;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        groundCheck = transform.Find("GroundCheck");
+        groundLayer = LayerMask.GetMask("Ground"); // Make sure to set your ground objects to the "Ground" layer
+        if (groundCheck == null)
+        {
+            Debug.LogError("GroundCheck object not found! Make sure you have an empty GameObject named 'GroundCheck' as a child of your player GameObject.");
+        }
+    }
+
+    private void Update()
+    {
+        // Check if the player is on the ground
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        // Player movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        rb.velocity = movement;
+
+        // Jumping
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+}
