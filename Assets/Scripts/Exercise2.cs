4e91053c0e898747e4144ce02676c7d8d7abf86a
JaniJuhaniKesola <jani.kesola@tuni.fi>
2024-01-26 09:57:52 +0200

    1-2 Done

---
+++ Assets/Scripts/Exercise2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise2 : MonoBehaviour
+{
+    private int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (i <= 1000) {
+        //    Debug.Log("Frame: " + i);
+        //    i++;
+        //}
+    }
+}
