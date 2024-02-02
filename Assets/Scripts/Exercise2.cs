490d1b03e5f6980ac396970a33d2190208564ad4
Simo Alanne <simo.alanne@tuni.fi>
2024-02-02 09:51:10 +0200

    Fibonacci wasn't done correctly

--- Assets/Scripts/Exercise2.cs
+++ Assets/Scripts/Exercise2.cs
@@ -1,21 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Exercise2 : MonoBehaviour
 {
-    private int _i;
+    private int _a = 0, _b = 1;
 
     void Start()
     {
-        Debug.Log("Hello World!");
+        Debug.Log("Hello World");
     }
 
     void Update()
-    {
-        if (_i <= 1000) {
-            Debug.Log("Frame: " + _i);
-            _i++;
+    {    
+        if (_a <= 1000)
+        {
+            Debug.Log(_a);
+            int temp = _a;
+            _a = _b;
+            _b = temp + _b;
         }
     }
 }
